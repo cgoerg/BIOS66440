94732f8ba9d9934712b7057b6097ea7c50d0ae88
Carsten Görg <goerg@CIDA-M-0V2JV40.local>
2024-12-05 13:59:17 -0700

    initial commit

---
+++ grep.R
@@ -0,0 +1,13 @@
+drug <- c("","Bup","Nal","Bup,Nal")
+df <- data.frame(drug)
+
+b_drug_index <- grep("Bup",df$drug) 
+n_drug_index <- grep("Nal",df$drug) 
+
+bup <- rep(0,4)  # initialize bup with zeros
+nal <- rep(0,4)  # initialize nal with zeros
+bup[b_drug_index] <- 1  # use index to set bup rowx to 1
+nal[n_drug_index] <- 2  # use index to set nal rows to 2
+dx <- bup + nal         # add indices: rows with only bup remain 1 (1+0), rows with only nal remain 2(0+2), rows with both bup and nal will be 3 (1+2)
+
+dx
